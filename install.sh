0037119b3b3ce7fe9e84c01cc4f9b41bef67a708
gwimtux <g5l@protonmail.com>
2022-11-22 16:56:42 -0800

    sdfa

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 
-pacman -Syyu --noconfirm
-pacman -S rofi-emoji --noconfirm
-pacman -S alacritty --noconfirm
-pacman -S rofi --noconfirm
-pacman -S rofi-calc --noconfirm
-pacman -S papirus-icon-theme-git --noconfirm
-pacman -S python-pip --noconfirm
+sudo pacman -Syyu --noconfirm
+sudo pacman -S rofi-emoji --noconfirm
+sudo pacman -S alacritty --noconfirm
+sudo pacman -S rofi --noconfirm
+sudo pacman -S rofi-calc --noconfirm
+sudo pacman -S papirus-icon-theme-git --noconfirm
+sudo pacman -S python-pip --noconfirm
 pip install --user bumblebee-status
-mkdir .fonts
-mv /home/$USER/dotfiles/config /home/$USER/.config
+mv /home/$USER/dotfiles/config/* /home/$USER/.config
 mv /home/$USER/dotfiles/fonts /home/$USER/.fonts
 
