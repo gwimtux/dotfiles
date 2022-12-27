1e3cb04ec725a8a3659380950d66d3bd15d2a5a0
gwimtux <g5l@protonmail.com>
2022-12-27 06:50:12 +0000

    changed some stuff

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ sudo pacman -S papirus-icon-theme-git --noconfirm
 sudo pacman -S python-pip --noconfirm
 pip install --user bumblebee-status
 sudo mkdir -p /usr/local/share/fonts
+sudo rm -rf /home/$USER/.config/*
 sudo mv /home/$USER/dotfiles/config/* /home/$USER/.config
 sudo mv /home/$USER/dotfiles/fonts/* /usr/local/share/fonts/
-sudo mv -f /home/$USER/dotfiles/gruvbox-dark.rasi /usr/share/rofi/themes/
+sudo mv /home/$USER/dotfiles/gwim.rasi /usr/share/rofi/themes/
 
